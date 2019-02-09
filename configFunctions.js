796854e06c636db6d83e4f657c9c16bba147bc8a
Wesley Ramalho <wesley_raa@hotmail.com>
2019-02-09 13:34:03 -0200

    configureIanes method with new parameters to config request interceptors

--- configFunctions.js
+++ configFunctions.js
@@ -1,33 +1,51 @@
 import axios from "axios";
+import { interceptorForRequest, interceptorForResponse } from "./interceptors";
+import { DEFAULT_CONTENT_TYPE, DEFAULT_TIMEOUT } from "./constants";
+import {
+  defaultBeforeTheRequestIsSentHandler,
+  defaultRequestErrorHandler,
+  defaultErrorResponseHandler,
+  defaultSuccessResponseHandler
+} from "./requestHandlers";
 
 export const setBaseUrl = url => {
   if (!url) return null;
-  axios.defaults.baseURL = url;
+  return (axios.defaults.baseURL = url);
 };
 
 export const setAuthorizationHeader = token => {
   if (!token) return null;
-  axios.defaults.headers.common["Authorization"] = token;
+  return (axios.defaults.headers.common["Authorization"] = token);
+};
+export const resetAuthorizationHeader = () => {
+  delete axios.defaults.headers.common["Authorization"];
+  return axios.defaults.headers.common["Authorization"];
 };
 
 export const setDefaultContentType = contentType => {
   if (!contentType) return null;
-  axios.defaults.headers.post["Content-Type"] = contentType;
+  return (axios.defaults.headers.post["Content-Type"] = contentType);
 };
 
 export const setDefaultTimeOut = timeout => {
   if (!timeout) return null;
-  axios.defaults.timeout = timeout;
+  return (axios.defaults.timeout = timeout);
 };
 
-export const configureIanes = (
-  baseUrl = "",
-  token = "",
-  defaultContentType = "application/json",
-  timeOut = 240000
-) => {
-  setBaseUrl(baseUrl);
-  setAuthorizationHeader(token);
+export const configureIanes = ({
+  apiUrl = "",
+  apiToken = "",
+  defaultContentType = DEFAULT_CONTENT_TYPE,
+  defaultTimeout = DEFAULT_TIMEOUT,
+  handleBeforeTheRequestIsSent = defaultBeforeTheRequestIsSentHandler,
+  handleRequestError = defaultRequestErrorHandler,
+  handleResponseDataSuccess = defaultSuccessResponseHandler,
+  handleResponseError = defaultErrorResponseHandler
+}) => {
+  setBaseUrl(apiUrl);
+  setAuthorizationHeader(apiToken);
   setDefaultContentType(defaultContentType);
-  setDefaultTimeOut(timeOut);
+  setDefaultTimeOut(defaultTimeout);
+  interceptorForRequest(handleBeforeTheRequestIsSent, handleRequestError);
+  interceptorForResponse(handleResponseDataSuccess, handleResponseError);
 };
