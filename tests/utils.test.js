418538abb2db5de29e5bb681b0a896bc4485fbc8
Wesley Ramalho <wesley_raa@hotmail.com>
2019-02-10 13:18:17 -0200

    utils 100% tested

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,25 +1,42 @@
-import { isNumber } from "../utils";
+import * as utils from "../utils";
 
 describe("possibles values to isNumber function", () => {
   test("passing a string to isNumber function", () => {
-    expect(isNumber("1234")).toEqual(false);
+    expect(utils.isNumber("1234")).toEqual(false);
   });
   test("passing a integer number to isNumber function", () => {
-    expect(isNumber(1234)).toEqual(true);
+    expect(utils.isNumber(1234)).toEqual(true);
   });
 });
 
 describe("testing hasCompleteResponse possible values", () => {
-  let hasCompleteResponseMockFunction = jest.fn();
-  const response = {
-    status: 444,
-    statusText: "No response",
-  };
-  hasCompleteResponseMockFunction(response);
-  expect(hasCompleteResponseMockFunction).toBeCalledWith(
-    expect.objectContaining({
-      status: expect.any(Number),
-      statusText: expect.any(String)
-    })
+  const hasCompleteResponseMockFunction = jest.spyOn(
+    utils,
+    "hasCompleteResponse"
   );
+  
+  test("passing right response object as parameter", () => {
+    const completeResponse = {
+      status: 444,
+      statusText: "No response"
+    };
+    hasCompleteResponseMockFunction(completeResponse);
+    expect(hasCompleteResponseMockFunction).toBeCalledWith(
+      expect.objectContaining({
+        status: expect.any(Number),
+        statusText: expect.any(String)
+      })
+    );
+  });
+
+  test("passing wrong response (incomplete) object as parameter", () => {
+    const incompleteResponse = null;
+    hasCompleteResponseMockFunction(incompleteResponse);
+    expect(hasCompleteResponseMockFunction).toBeCalledWith(
+      expect.objectContaining({
+        status: expect.any(Number),
+        statusText: expect.any(String)
+      })
+    );
+  });
 });
