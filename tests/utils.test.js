52681e3337e172d4a2134c255ec52c6d5520dd35
Wesley Ramalho <wesley_raa@hotmail.com>
2019-02-10 12:59:34 -0200

    testing hasCompleteResponse properties

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -8,3 +8,18 @@ describe("possibles values to isNumber function", () => {
     expect(isNumber(1234)).toEqual(true);
   });
 });
+
+describe("testing hasCompleteResponse possible values", () => {
+  let hasCompleteResponseMockFunction = jest.fn();
+  const response = {
+    status: 444,
+    statusText: "No response",
+  };
+  hasCompleteResponseMockFunction(response);
+  expect(hasCompleteResponseMockFunction).toBeCalledWith(
+    expect.objectContaining({
+      status: expect.any(Number),
+      statusText: expect.any(String)
+    })
+  );
+});
--- utils.js
+++ utils.js
@@ -1,1 +1,6 @@
 export const isNumber = variable => typeof variable === "number";
+
+export const hasCompleteResponse = response => {
+  if (!!response && !!response.status && !!response.statusText) return true;
+  return false;
+};
