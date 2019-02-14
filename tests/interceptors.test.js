c66235ad48d0deacb83ffe0359af22013bc37a2e
Wesley Ramalho <wesley_raa@hotmail.com>
2019-02-14 14:29:14 -0200

    img added on readme and coverage update

--- tests/interceptors.test.js
+++ tests/interceptors.test.js
@@ -1,6 +1,19 @@
 // import { interceptorForRequest, interceptorForResponse } from "../interceptors";
 import * as interceptors from "../interceptors";
 
+
+describe("testing interceptor for response", () => {
+  const defaultInterceptorForResponse = jest.spyOn(
+    interceptors,
+    "interceptorForResponse"
+  );
+  defaultInterceptorForResponse();
+  test("checking if return is ok", () => {
+    expect(defaultInterceptorForResponse).any(Function);
+  });
+
+});
+
 describe("testing interceptor for request", () => {
   const defaultInterceptorForRequest = jest.spyOn(
     interceptors,
@@ -8,7 +21,8 @@ describe("testing interceptor for request", () => {
   );
   defaultInterceptorForRequest();
   test("checking if return is ok", () => {
-    expect(defaultInterceptorForRequest.resol).toBeInstanceOf(Function);
+    expect(defaultInterceptorForRequest).any(Function);
   });
 });
-// describe("testing interceptor for response", () => {});
+
+
